c4957b5d5c863c0aaaaf97d5430895ea66f7108c
Dat Nguyen <mrdat0194@gmail.com>
2026-04-16 16:39:27 +0700

    refactor: datastructure

--- datastructure/test/story_teller_test.py
+++ datastructure/test/story_teller_test.py
@@ -1,6 +1,8 @@
 from datastructure.story_teller import story_teller
 
 import unittest
+from unittest.mock import patch
+from io import StringIO
 
 class TestStory(unittest.TestCase):
 
@@ -17,6 +19,49 @@ class TestStory(unittest.TestCase):
         arr = [[1, 5], [1, 6], [3, 2], [1, 10], [1, 10], [1, 6], [2, 5], [3, 2]]
         res = story_teller.freqQuery(arr)
         self.assertEqual(res, [0, 1])
+
+    def test_biggest_bst(self):
+        newNode = story_teller.newNode
+        largestBST = story_teller.largestBST
+
+        self.assertEqual(largestBST(None), 0)
+
+        root = newNode(10)
+        self.assertEqual(largestBST(root), 1)
+
+        root = newNode(20)
+        root.left = newNode(10)
+        root.right = newNode(30)
+        self.assertEqual(largestBST(root), 3)
+
+        root = newNode(50)
+        root.left = newNode(10)
+        root.right = newNode(60)
+        root.left.left = newNode(5)
+        root.left.right = newNode(20)
+        root.right.left = newNode(55)
+        root.right.left.left = newNode(45)
+        root.right.right = newNode(70)
+        root.right.right.left = newNode(65)
+        root.right.right.right = newNode(80)
+        self.assertEqual(largestBST(root), 6)
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_printArray(self, mock_stdout):
+        vInt = [1, 2, "a"]
+        res = story_teller.printArray(vInt)
+        output = mock_stdout.getvalue().strip().split('\n')
+        self.assertEqual(output, ["1", "2", "a"])
+        self.assertEqual(res, "a")
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_two_sum_hashing(self, mock_stdout):
+        num_arr = [4, 5, 5, 1, 8]
+        pair_sum = 9
+        story_teller.twoSumHashing(num_arr, pair_sum)
+        output = mock_stdout.getvalue().strip()
+        self.assertIn("Pair with sum 9 is: ( 5 , 4 )", output)
+        self.assertIn("Pair with sum 9 is: ( 8 , 1 )", output)
 
 
 
